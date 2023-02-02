fad7f6b9e2356c28307729d1ff9d5fa84b35dcbf
NeverWalkAloner <neverwalkaloner@gmail.com>
2023-02-02 11:37:10 +0500

    Add crypto tools

---
+++ app/core/crypto_tools.py
@@ -0,0 +1,75 @@
+import base64
+
+from cryptography.hazmat.primitives import hashes, padding, serialization
+from cryptography.hazmat.primitives.asymmetric import padding as asym_padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric.rsa import (
+    RSAPrivateKey,
+    RSAPublicKey,
+)
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+def symmetric_encryption(message: str) -> str:
+    """Encrypt message using symmetric encryption."""
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(message.encode('utf-8'))
+    padded_data += padder.finalize()
+    cipher = Cipher(algorithms.AES(b'1234567890123456'), modes.ECB())
+    encryptor = cipher.encryptor()
+    ct = encryptor.update(padded_data) + encryptor.finalize()
+    return base64.b64encode(ct).decode('utf-8')
+
+
+def symmetric_decryption(ciphertext: str) -> str:
+    """Decrypt message using symmetric encryption."""
+    cipher = Cipher(algorithms.AES(b'1234567890123456'), modes.ECB())
+    decryptor = cipher.decryptor()
+    message = (
+        decryptor.update(base64.b64decode(ciphertext)) + decryptor.finalize()
+    )
+    unpadder = padding.PKCS7(128).unpadder()
+    data = unpadder.update(message)
+    data += unpadder.finalize()
+    return data.decode('utf-8')
+
+
+def generate_asymmetric_keys() -> RSAPrivateKey:
+    """Generate new RSA keypair."""
+    return rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+
+
+def serialize_private_key(keypair: RSAPrivateKey) -> str:
+    """Serialize private key and return PEM format."""
+    pem = keypair.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+    return pem.decode('utf-8')
+
+
+def serialize_public_key(keypair: RSAPrivateKey) -> str:
+    """Serialize public key and return PEM format."""
+    public_key = keypair.public_key()
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+    return pem.decode('utf-8')
+
+
+def asymmetric_encryption(message: str, public_key: RSAPublicKey) -> str:
+    """Encrypt message using RSA public key."""
+    ciphertext = public_key.encrypt(
+        message.encode('utf-8'),
+        asym_padding.OAEP(
+            mgf=asym_padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None,
+        ),
+    )
+    return base64.b64encode(ciphertext).decode('utf-8')
--- app/models/users.py
+++ app/models/users.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy_utils import EmailType, PasswordType, force_auto_coercion
 
 from app.db.base_class import Base
@@ -13,3 +14,16 @@ class User(Base):
     name = Column(String(50))
     email = Column(EmailType(50), unique=True, nullable=False)
     password = Column(PasswordType(schemes=["pbkdf2_sha512"]), nullable=False)
+
+    keys = relationship("UserKeys", back_populates="user", lazy='dynamic')
+
+
+class UserKeys(Base):
+    __tablename__ = "user_keys"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    public_key = Column(String(2000), nullable=False)
+    is_revoked = Column(Boolean, default=False)
+
+    user = relationship("User", back_populates="keys")
