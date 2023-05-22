dc7ca2c9f34df4b7c7b8e7149d8abee5a5b0f5c4
NeverWalkAloner <neverwalkaloner@gmail.com>
2023-05-22 13:25:45 +0500

    Add websocket client

---
+++ websocket_test_client/main.py
@@ -0,0 +1,11 @@
+from websockets.sync.client import connect
+
+
+def hello():
+    with connect("ws://localhost:8765") as websocket:
+        websocket.send("Hello world!")
+        message = websocket.recv()
+        print(f"Received: {message}")
+
+
+hello()
